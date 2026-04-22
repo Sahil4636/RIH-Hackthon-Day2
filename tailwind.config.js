1fb3b6eb827095772a6255908be802138f514164
siddharthjawade20@gmail.com <siddharthjawade20@gmail.com>
2026-04-22 10:02:48 +0530

    Add Tailwind CSS configuration with a premium dark theme

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        electricBlue: '#007BFF',
+        teal: '#008080',
+        yellow: '#FFD700',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
